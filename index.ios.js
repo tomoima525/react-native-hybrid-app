b07d995c44cdec1e186d3fff95668206ef375afa
tomoima525 <tomoima525@gmail.com>
2017-12-16 14:15:58 -0800

    boolean type check, redux devtool

--- index.ios.js
+++ index.ios.js
@@ -8,15 +8,17 @@ import React,{Component} from 'react';
 import {Provider} from 'react-redux';
 import {createStore, applyMiddleware} from 'redux';
 import {createEpicMiddleware} from 'redux-observable';
+import { composeWithDevTools } from 'remote-redux-devtools';
 import chuckNorris from './js/reducers';
 import chuckEpic from './js/epics';
 import App from './js/App';
 
 import {AppRegistry} from 'react-native';
 
+const composeEnhancers = composeWithDevTools({ realtime: true, port: 8000 });
 const epicMiddleware = createEpicMiddleware(chuckEpic);
 
-const store = createStore(chuckNorris, applyMiddleware(epicMiddleware));
+const store = createStore(chuckNorris, composeEnhancers(applyMiddleware(epicMiddleware)));
 
 export default class ChuckNorrisViewer extends Component {
 
