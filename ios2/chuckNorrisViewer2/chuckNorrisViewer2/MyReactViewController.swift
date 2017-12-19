11af5910d178b70ded5f87f38c10561c48f387c9
tomoima525 <tomoima525@gmail.com>
2017-12-18 17:26:47 -0800

    setup pod

--- ios2/chuckNorrisViewer2/chuckNorrisViewer2/MyReactViewController.swift
+++ ios2/chuckNorrisViewer2/chuckNorrisViewer2/MyReactViewController.swift
@@ -7,7 +7,22 @@
 //
 
 import UIKit
+import React
 
 class MyReactViewController : UIViewController {
-    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let jsCodeLocation = URL(string: "http://localhost:8081/index.bundle?platform=ios")
+        
+        let rootView = RCTRootView(
+            bundleURL: jsCodeLocation,
+            moduleName: "ChuckNorrisViewer",
+            initialProperties: nil,
+            launchOptions: nil
+        )
+        let vc = UIViewController()
+        vc.view = rootView
+        self.present(vc, animated: true, completion: nil)
+    }
 }
